b93ecf4c2baa224b91e0f37a3da3ea747781dae1
ClairVoyanceMedium <contact.clairvoyancemedium@gmail.com>
2024-12-21 16:19:57 +0100

    Update Fichiers.js

--- Netifly.fonctions/Fichiers.js
+++ Netifly.fonctions/Fichiers.js
@@ -1,4 +1,4 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require('stripe')(process.env.sk_live_qFFqmqh3jYq4iczMGXnf9qZk);
 
 exports.handler = async (event) => {
     const sig = event.headers['stripe-signature'];
